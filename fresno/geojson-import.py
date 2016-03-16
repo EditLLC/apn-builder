7ae001aad113e1dd88a748498c16641fb0ed8a00
karvel <jadeilyn@yahoo.com>
2016-03-16 14:35:25 -0700

    Revised script to upload in batches of 200.

--- fresno/geojson-import.py
+++ fresno/geojson-import.py
@@ -1,9 +1,10 @@
 import montage
 import rapidjson
 
-client = montage.Client('apn-builder', 'API')
+client = montage.Client('apn-builder', '7b1e530a0a251598bd81960628bd8d3b56fd20c6')
 apns = rapidjson.loads(open('fresno-parcels.json', 'r').read())
 total = len(apns)
+batch = []
 
 for index, (apn, boundary) in enumerate(apns.items()):
     document = {
@@ -12,9 +13,16 @@ for index, (apn, boundary) in enumerate(apns.items()):
         'county': 'Fresno',
         'boundary': boundary
     }
+    batch.append(document)
+
+    if len(batch) == 200:
+        client.documents.save('apn', *batch)
+        batch = []
     print("{index} of {total} - {apn}".format(
         index=index,
         total=total,
         apn=apn,
     ))
-    client.documents.save('apn', document)
+
+if batch:
+    client.documents.save('apn', *batch)
